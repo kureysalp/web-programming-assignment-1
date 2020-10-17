05b908cd0119465b848dac527980c85feac384b8
Alp Kureyş Kilic <kureysalp@gmail.com>
2020-10-17 15:58:02 +0300

    Adjustments

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ server.listen(port, () => {
 })
 
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 })
 
 server.get("/welcome", (req, res) => {
